a61483e73fff047db1ac4d1dc6e386617256b219
witt3rd <witt3rd@witt3rd.com>
2020-11-22 09:44:37 -0800

    +foaf; thumbnails

--- scripts/cvt_all.sh
+++ scripts/cvt_all.sh
@@ -1,6 +1,7 @@
 
 #!/usr/bin/env bash
 
-./rdfcsv.py -i ontologies/rdf.ttl -f ttl -o data/rdf.csv
-./rdfcsv.py -i ontologies/rdfs.ttl -f ttl -o data/rdfs.csv
-./rdfcsv.py -i ontologies/owl.ttl -f ttl -o data/owl.csv
+scripts/rdfcsv.py -i ontologies/rdf.ttl -f ttl -o data/rdf.csv
+scripts/rdfcsv.py -i ontologies/rdfs.ttl -f ttl -o data/rdfs.csv
+scripts/rdfcsv.py -i ontologies/owl.ttl -f ttl -o data/owl.csv
+scripts/rdfcsv.py -i ontologies/foaf.rdf -f xml -o data/foaf.csv
